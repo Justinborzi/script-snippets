a1ee39d6b70e79c800b1c7ca6ddc8ac56ef903fd
MysticSeagull <Justinborzi@gmail.com>
2020-01-08 21:57:23 -0500

    Add files via upload

---
+++ CloudMovement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float speed = 2.1f;
+    int current = 1;
+    readonly float wPradius = 1f;
+
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < wPradius)
+        {
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+}
---
+++ DevDisplay.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DevDisplay : MonoBehaviour
+{
+    public Canvas devCanvas;
+    public Font devFont;
+    public int sizeForFont = 20;
+
+    Text text;
+    RectTransform rectTransform;
+    float deltaTime = 0.0f;
+
+    void Start()
+    {
+        // Initialize Game Object
+        GameObject fpsCounter = new GameObject();
+        fpsCounter.transform.parent = devCanvas.transform;
+        fpsCounter.name = "devText";
+        text = fpsCounter.AddComponent<Text>();
+        text.font = devFont;
+        text.fontSize = sizeForFont;
+
+        // Text position
+        rectTransform = text.GetComponent<RectTransform>();
+        rectTransform.localPosition = new Vector3(0, 0, 0);
+        rectTransform.sizeDelta = new Vector2(1200, 600);
+    }
+
+    void Update()
+    {
+        // Intialize FPS Values
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+
+
+        text.text = string.Format(
+             "{0} FPS [{1} MS]" + Environment.NewLine +
+             "{2} [{3}]" + Environment.NewLine +
+             "{4} MB VRAM" + Environment.NewLine +
+             "{5}" + Environment.NewLine +
+             "{6} MB RAM" + Environment.NewLine +
+             "{7}",
+
+
+             Math.Round(fps), 
+             msec,
+             SystemInfo.graphicsDeviceName,
+             SystemInfo.graphicsDeviceType.ToString(),
+             SystemInfo.graphicsMemorySize.ToString(),
+             SystemInfo.processorType,
+             SystemInfo.systemMemorySize.ToString(),
+             SystemInfo.operatingSystem
+         );
+    }
+}
