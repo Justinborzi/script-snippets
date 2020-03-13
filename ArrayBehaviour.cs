f5016a697bd68e1173565cf0e196a24f56f48ad8
MysticSeagull <Justinborzi@gmail.com>
2020-03-12 21:12:03 -0400

    Add files via upload

---
+++ ArrayBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class NamedArray : PropertyAttribute
+{
+    public readonly string[] names;
+    public NamedArray(string[] names) { this.names = names; }
+}
+
+[CustomPropertyDrawer(typeof(NamedArray))]
+public class NamedArrayDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+    {
+        try
+        {
+            int pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
+            EditorGUI.PropertyField(rect, property, new GUIContent(((NamedArray)attribute).names[pos]));
+        }
+        catch
+        {
+            EditorGUI.PropertyField(rect, property, label);
+        }
+    }
+}
